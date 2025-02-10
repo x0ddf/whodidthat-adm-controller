fc8ab040820831d530fc3e2065c45d6c178e1fe2
Sergey Yakovlev <noctus@proton.me>
2025-02-10 10:32:46 +0400

    update tls

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/x0ddf/whodidthat-controller/controllers"
@@ -10,19 +12,27 @@ import (
 
 func main() {
 	var port int
+	var tlsKey, tlsCert string
 	flag.IntVar(&port, "port", 8443, "The port to listen on")
+	flag.StringVar(&tlsKey, "tls-key", "/etc/webhook/certs/tls.key", "Private key for TLS")
+	flag.StringVar(&tlsCert, "tls-crt", "/etc/webhook/certs/tls.crt", "TLS certificate")
 	flag.Parse()
-
+	certs, err := tls.LoadX509KeyPair(tlsCert, tlsKey)
+	if err != nil {
+		log.Fatalf("fail to load tls certificates:%v", err)
+	}
 	ac := controllers.NewAdmissionController()
 
 	http.HandleFunc("/mutate", ac.Handle)
 
-	// You'll need to set up TLS certificates for production use
 	server := &http.Server{
 		Addr: fmt.Sprintf(":%d", port),
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{certs},
+		},
 	}
 
 	if err := server.ListenAndServe(); err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 }
